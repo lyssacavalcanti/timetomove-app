9f22032540b318be160de8fb191665016876a281
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-02-24 00:02:19 -0300

    Initial files upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button color="red"> Botão 1 </Button>
+      <Button color="blue"> Botão 2 </Button>
+      <Button color="green"> Botão 3 </Button>
+    </div>
+  );
+}
+
+export default App;
