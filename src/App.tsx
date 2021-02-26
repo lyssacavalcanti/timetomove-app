e0bf7df6f99a1d3e58177b4afa0ee2d08ca6be6a
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-02-25 23:41:13 -0300

    Inserted a function to increment the numbers of the buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Button } from "./components/Button";
 function App() {
   return (
     <div>
+      <img src="" alt="" />
       <Button color="red"> Botão 1 </Button>
       <Button color="blue"> Botão 2 </Button>
       <Button color="green"> Botão 3 </Button>
