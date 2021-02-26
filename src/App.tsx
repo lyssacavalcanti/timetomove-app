411c7088a1f762c6fbc11a2d8a1ff0addea328a5
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-02-26 01:31:05 -0300

    Set initial  components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { Button } from "./components/Button";
+import { ExperienceBar } from "./components/ExperienceBar";
+import "./styles/global.css";
 
 function App() {
   return (
-    <div>
-      <img src="" alt="" />
-      <Button color="red"> Botão 1 </Button>
-      <Button color="blue"> Botão 2 </Button>
-      <Button color="green"> Botão 3 </Button>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
